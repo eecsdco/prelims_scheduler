22196e8c93ac6c44d9053b6d34318be85b3ceb56
Chris Puzzuoli <cpuzzuol@umich.edu>
2021-11-16 11:27:47 -0800

    add dev_user.py file

--- prelims/views.py
+++ prelims/views.py
@@ -205,7 +205,7 @@ def login_view(request):
         request.session['uniqname'] = uniqname
         return HTTPFound(location='/calendar.html')
     except KeyError:
-        return {'why_failed': request.session}
+        return {'why_failed': 'uniqname is required'}
     except NoResultFound:
         return {'why_failed': uniqname + ' is not a faculty uniqname. If yours is missing, please e-mail Ashley (smash@umich.edu)'}
     return {'why_failed': ''}
